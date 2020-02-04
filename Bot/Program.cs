fd6972317703df2dd5b5e859b5edef1d1780e5e3
Junior Dev <flavio.junior@milanleiloes.com.br>
2020-02-04 18:27:15 -0300

    Configuração Geral !!

---
+++ Bot/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
+using prmToolkit.Selenium;
+using prmToolkit.Selenium.Enum;
+
+
+namespace Bot
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            IWebDriver webDriver = WebDriverFactory.CreateWebDriver(Browser.Firefox, @"C:\Selenium\FirefoxDriver\", true);
+            int i = 0;
+            while (true)
+            {
+                CarregarPagina(webDriver);
+                Console.WriteLine(i++);
+            }
+        }
+
+        public static void CarregarPagina(IWebDriver webDriver)
+        {
+            try
+            {
+                webDriver.LoadPage(TimeSpan.FromSeconds(1), "www.google.com.br");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Thread.Sleep(1000);
+        }
+    }
+}
